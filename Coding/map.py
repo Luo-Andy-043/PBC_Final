3d0501c17ffaef8280ea96cc1d1f1c7cab636cba
b08703114 <75960599+b08703114@users.noreply.github.com>
2020-12-26 21:46:20 +0800

    update wall

--- Coding/map.py
+++ Coding/map.py
@@ -11,10 +11,19 @@ class Map:
         with open(filename, 'rt') as f:
             for line in f:
                 self.data.append(line)
+        #print(self.data)
         self.tilewidth = len(self.data[0])
         self.tileheight = len(self.data)
         self.width = self.tilewidth * TILESIZE
         self.height = self.tileheight * TILESIZE
+        self.map_data=[]
+        for i in range(len(self.data)):
+            self.map_data.append(self.data[i][:48])
+            #print(self.string)
+        #print(self.data)
+        #for i in range(len(self.data)):
+        #    print(self.map_data[i])
+
 
 
 class Camera:
